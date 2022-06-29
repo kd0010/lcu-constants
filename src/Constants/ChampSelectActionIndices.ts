465d293fb0368ed76e4ed9bd9920bfaabf77917d
David Kanders <kanders.davids@gmail.com>
2022-06-29 16:16:27 +0300

    add `ChampSelectActionIndices`

---
+++ src/Constants/ChampSelectActionIndices.ts
@@ -0,0 +1,14 @@
+/**
+ * Indices that correspond to a certain category
+ * in `ChampSelectSessionEventData.actions`.
+ */
+export const ChampSelectActionIndices = {
+  banActions: 0,
+  tenBansRevealAction: 1,
+  picksB1: 2,
+  picksR1R2: 3,
+  picksB2B3: 4,
+  picksR3R4: 5,
+  picksB4B5: 6,
+  picksR5: 7,
+} as const
